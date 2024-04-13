973ec0d3723e54a12c11841b313b8bd8bd93f5c3
Iryna Kakalets <irinakakalec@gmail.com>
2024-04-13 11:09:58 +0200

    uprava a hotove

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const minutka = document.querySelector('.alarm');
-const zvuk = document.querySelector('audio');
-const stopTlacitko = document.querySelector('button');
-const vstupOdUzivatele = Number(
-  prompt('Zadej za jak dlouho má zazvonit minutka:'),
-);
-const zvoneni = () => {
-  minutka.classList.add('alarm--ring');
-  zvuk.play();
-};
+'use strict';
 
-const casovac = setTimeout(zvoneni, vstupOdUzivatele * 1000);
-stopTlacitko.addEventListener('click', () => {
-  clearTimeout(casovac);
+console.log('funguju!');
+
+
+const formElm = document.querySelector('.controls');
+
+formElm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const timeInput = Number(document.querySelector('.time-input').value);
+  let time = timeInput * 60;
+
+  const countdownInterval = setInterval(() => {
+    const minutes = Math.floor(time / 60); // Získáme celé minuty
+    const seconds = time % 60; // Získáme zbývající sekundy
+
+    document.querySelector('.alarm__minutes').textContent = String(
+      minutes,
+    ).padStart(2, '0');
+    document.querySelector('.alarm__seconds').textContent = String(
+      seconds,
+    ).padStart(2, '0');
+
+    time -= 1;
+
+    if (time === -1) {
+      clearInterval(countdownInterval);
+      document.querySelector('audio').play();
+    }
+  }, 1000);
 });
